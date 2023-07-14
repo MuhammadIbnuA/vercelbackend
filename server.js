774cf090f5b78fdca9735a94d46d68494e02dfc0
MuhammadIbnuA <129247263+MuhammadIbnuA@users.noreply.github.com>
2023-07-14 11:31:29 +0700

    6wlogin

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 
 const app = express();
 app.use(express.json());
@@ -44,6 +45,22 @@ const menuSchema = new mongoose.Schema({
 });
 const Menu = mongoose.model("Menu", menuSchema);
 
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
 // Authentication middleware
 const isAuthenticated = (req, res, next) => {
   try {
@@ -59,7 +76,67 @@ const isAuthenticated = (req, res, next) => {
     return res.status(401).json({ error: "Invalid token" });
   }
 };
+// User controller
+// Register a new user
+const registerUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
+    // Check if username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+
+    // Save the user to the database
+    await newUser.save();
+
+    res.json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+// Login logic
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // Check password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign({ user: user.username }, 'hakunamatata', { expiresIn: '1h' })
+
+    // Set the JWT token as a cookie
+    res.cookie('token', token, { maxAge: 3600000, httpOnly: true }); // Expiry set to 1 hour (3600000 milliseconds)
+
+    // Return the token as a response
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
 // Menu controller
 // Create a new menu
 async function createMenu(req, res) {
@@ -153,6 +230,10 @@ app.put("/api/menu/:id", updateMenu);
 
 // Delete a menu by ID
 app.delete("/api/menu/:id", deleteMenu);
+
+app.post("/api/login", login)
+
+app.post("/api/register", registerUser)
 
 // Start the server
 connect().then(() => {
